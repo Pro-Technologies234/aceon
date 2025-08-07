fbaa130c987f8cce4db0066df81933e331f910be
Pro-Technologies234 <protechnologies332@gmail.com>
2025-08-08 00:31:13 +0100

    fixed font wrong import name

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,8 +6,8 @@ const cabinetGrotesk = localFont({
   src: [
     { path: '../assets/fonts/cabinet-grotesk/CabinetGrotesk-Black.woff2', weight: '900', style: 'normal' },
     { path: '../assets/fonts/cabinet-grotesk/CabinetGrotesk-Bold.woff2', weight: '700', style: 'normal' },
-    { path: '../assets/fonts/cabinet-grotesk/CabinetGrotesk-ExtraBold.woff2', weight: '800', style: 'normal' },
-    { path: '../assets/fonts/cabinet-grotesk/CabinetGrotesk-ExtraLight.woff2', weight: '200', style: 'normal' },
+    { path: '../assets/fonts/cabinet-grotesk/CabinetGrotesk-Extrabold.woff2', weight: '800', style: 'normal' },
+    { path: '../assets/fonts/cabinet-grotesk/CabinetGrotesk-Extralight.woff2', weight: '200', style: 'normal' },
     { path: '../assets/fonts/cabinet-grotesk/CabinetGrotesk-Light.woff2', weight: '300', style: 'normal' },
     { path: '../assets/fonts/cabinet-grotesk/CabinetGrotesk-Medium.woff2', weight: '500', style: 'normal' },
     { path: '../assets/fonts/cabinet-grotesk/CabinetGrotesk-Regular.woff2', weight: '400', style: 'normal' },
